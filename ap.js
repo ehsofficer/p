d64f1d12d18c20275fb2123aae9b33edfaf99707
Moiz <hajidon66@gmail.com>
2023-08-11 23:54:59 +0530

    commiting ap and p2

---
+++ ap.js
@@ -0,0 +1,7 @@
+var addBtn = document.getElementById("add-task")
+var addTask = document.getElementById("task")
+var addList = document.getElementById("tasks")
+
+addBtn.addEventListener function({
+    addBtn
+}) 
